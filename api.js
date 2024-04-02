0a55e19a2f7c3dc4e41e7e88aca2e5d471041b67
JianWXu <jianwxu88@gmail.com>
2024-04-01 20:08:11 -0700

    Update api.js

--- api.js
+++ api.js
@@ -1,8 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 
 // const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
 
-let BASE_URL = "http://localhost:3001";
+let BASE_URL = "https://joblyworld.onrender.com";
 
 /** API Class.
  *
